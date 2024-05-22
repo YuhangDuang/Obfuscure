e6a465897169f8e43c8887066cc58522a09cd2a2
duang4real <127297374+YuhangDuang@users.noreply.github.com>
2024-05-22 14:35:20 +0300

    Uploaded dashboard.js

---
+++ client/js/dashboard.js
@@ -0,0 +1,26 @@
+$("#stats").click(function() {
+    window.location.href = "stats.html";
+});
+$("#logout_facebook").click(function() {
+    var params = {
+        user_id: localStorage.getItem('userId')
+    }
+    $.ajax({  
+        url: "http://bot.1568888.xyz/settings.php?mod=logout_facebook",
+        method: 'POST',
+        dataType: 'json',
+        data: params,
+        success: function(data) {  
+            if(data.status=="success"){
+                localStorage.removeItem("Infacebook");
+                alert("Logged out successfully!");
+                window.location.href = "settings.html";
+            }else{
+                alert("Error Logged out");
+            }
+        },  
+        error: function(xhr, status, error) {  
+            console.error('Error fetching data: ' + error);  
+        } 
+    }); 
+});
